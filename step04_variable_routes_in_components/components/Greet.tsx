31a48fa7beb13129523c730e65619e72ce8d3078
Adil Altaf <khanadilaltaf@gmail.com>
2022-06-29 11:57:00 -0400

    added navbar links

--- step04_variable_routes_in_components/components/Greet.tsx
+++ step04_variable_routes_in_components/components/Greet.tsx
@@ -1,8 +1,5 @@
 import { useRouter } from "next/router";
 import type { NextComponentType, NextPageContext } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../../styles/Home.module.css";
 
 interface Props {}
 
